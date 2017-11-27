ad0f4879d06fbace1ad7914183c683facf05abfe
José R. Sousa <jose.r.sousa@sage.com>
2017-11-27 17:35:34 +0000

    - Atribuição de TDE caso cliente no final da venda tenha mais de 200Pontos; - Acesso à tabela UXDiscounColorAndSize (tem query para a sua criação) na validação das linhas para atribuir descontos sobre cores e tamanhos; - Apresentação do stock existente nos restantes armazéns caso o controlo de stock esteja ativo; - Atualização de preços de venda caso seja lançado um ORC.

---
+++ Sage.Retail.Extensibility.Sample4/Sage.Retail.Extensibility.Sample4/StockByWareHouse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RTLExtenderSample
+{
+    public class StockByWareHouse
+    {
+        //public string itemID { get; set; }
+        public double Stock { get; set; }
+        public short WareHouseID { get; set; }
+    }
+}
